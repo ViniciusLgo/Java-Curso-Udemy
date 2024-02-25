5218c76135e54d0a00cafe33a09978201f2c1616
ViniciusLgo <vinicruzlago@gmail.com>
2024-02-25 15:53:29 -0300

    aula 26 udemy

--- src/java_curso_udemy/a2_entradadedados.java
+++ src/java_curso_udemy/a2_entradadedados.java
@@ -1,9 +1,38 @@
 package java_curso_udemy;
 
+import java.util.Scanner;
+
 public class a2_entradadedados {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+	// Case 1 
+		
+		String x;
+		
+		System.out.println("Digite algo:");
+		x = sc.next();
+		System.out.println("vc digitou " + x );
+		
+		
+	// Case 2 
+		
+		int z;
+		
+		System.out.println("e agr digite um numero:");
+		z = sc.nextInt();
+		System.out.println("vc digitou " + z );
+		
+		
+	// Case 3
+		
+				double y;
+				
+				System.out.println("e agr digite outro numero:");
+				y = sc.nextDouble();
+				System.out.println("vc digitou " + y );
 
 	}
 
--- src/java_curso_udemy/a2_exercicios.java
+++ src/java_curso_udemy/a2_exercicios.java
@@ -1,9 +1,30 @@
 package java_curso_udemy;
 
+import java.util.Scanner;
+
 public class a2_exercicios {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+	// Case 1
+		
+		String s1, s2, s3;
+
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		sc.close();
+
+		// Case 2
+		
+		
 
 	}
 
